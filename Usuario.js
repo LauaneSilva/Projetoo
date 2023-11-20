1de546ca561d2186cf62a9fa514796987a55d496
LauaneSilva <132077022+LauaneSilva@users.noreply.github.com>
2023-11-20 09:57:51 -0300

    Delete Usuario.js

--- Usuario.js
+++
@@ -1,9 +0,0 @@
-class Usuario{
-    constructor(email, nome, senha){
-        this.email = email;
-        this.senha = senha;
-        this.nome = nome;
-        
-    }
-}
-module.exports = Usuario;
